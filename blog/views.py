5fcdcb8fb2f8a1b4ca06c5fdd3cbef2dc0dbc60a
0quasd <vova.pigovskyy@gmail.com>
2025-11-10 11:29:19 +0200

    comments: model, forms, views, urls, templates, moderation, counts

--- blog/views.py
+++ blog/views.py
@@ -1,41 +1,65 @@
 from django.shortcuts import render, get_object_or_404, redirect
-from django.utils import timezone
-from .forms import PostForm
-from .models import Post
-
+from django.contrib.auth.decorators import login_required
+from .models import Post, Comment
+from .forms import PostForm, CommentForm
+from django.db.models import Count, Q
 
 def post_list(request):
-    posts = Post.objects.order_by('-published_date', '-created_date')
+    posts = (Post.objects
+             .annotate(approved_count=Count('comments', filter=Q(comments__approved_comment=True)))
+             .order_by('-published_date'))
     return render(request, 'blog/post_list.html', {'posts': posts})
 
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
     return render(request, 'blog/post_detail.html', {'post': post})
 
+def add_comment_to_post(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    if request.method == 'POST':
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            comment = form.save(commit=False)
+            comment.post = post
+            comment.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = CommentForm()
+    return render(request, 'blog/add_comment_to_post.html', {'form': form})
+
+@login_required
+def comment_approve(request, pk):
+    comment = get_object_or_404(Comment, pk=pk)
+    comment.approve()
+    return redirect('post_detail', pk=comment.post.pk)
+
+@login_required
+def comment_remove(request, pk):
+    comment = get_object_or_404(Comment, pk=pk)
+    post_pk = comment.post.pk
+    comment.delete()
+    return redirect('post_detail', pk=post_pk)
+
+@login_required
 def post_new(request):
-    if request.method == "POST":
+    if request.method == 'POST':
         form = PostForm(request.POST)
         if form.is_valid():
-            post = form.save(commit=False)
-            post.author = request.user
-            post.published_date = timezone.now()
-            post.save()
-            return redirect("post_detail", pk=post.pk)
+            post = form.save()
+            return redirect('post_detail', pk=post.pk)
     else:
         form = PostForm()
-    return render(request, "blog/post_edit.html", {"form": form})
+    return render(request, 'blog/post_edit.html', {'form': form})
 
 
+@login_required
 def post_edit(request, pk):
     post = get_object_or_404(Post, pk=pk)
     if request.method == "POST":
         form = PostForm(request.POST, instance=post)
         if form.is_valid():
-            post = form.save(commit=False)
-            post.author = request.user
-            post.published_date = timezone.now()
-            post.save()
-            return redirect("post_detail", pk=post.pk)
+            post = form.save()
+            return redirect('post_detail', pk=post.pk)
     else:
         form = PostForm(instance=post)
-    return render(request, "blog/post_edit.html", {"form": form})
+    return render(request, 'blog/post_edit.html', {'form': form})
